21c939981c90d12dcb20d1940de13b19245fc511
Christerper <christerper91@gmail.com>
2021-04-21 11:42:00 +0800

    imrpove game choice & computer

--- application.rb
+++ application.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 class RockPaperScisor < Sinatra::Base
-enable :session
+  enable :session
   get '/' do
     erb :homepage
   end
@@ -18,21 +18,39 @@ enable :session
   get '/game' do
     erb :game
   end
-end
 
-#   get 'game/throw' do
-#     session['count'] = 0
-#     options = ['rock', 'paper', 'scisor']
-#     computer_choice = rand(options.length)
-#     player_choice = params [:player_choice]
-#     computer_answer = options [computer_choice]
-#     session['count'] += 1
-#     @count = session['count']
-#
-#     if player_choice == 'rock' && computer_answer == 'paper'
-#       #player lose
-#     elsif player_choice == 'paper' && computer_answer == 'scisor'
-#       #player lose
-#     elsif player_choice == 'scisor' && computer_answer == 'rock'
-#   end
-# end
+  get '/game/throw/:player_choice' do
+    session['count'] = 0
+    options = ['rock', 'paper', 'scisor']
+    computer_choice = rand(options.length)
+    player_choice = params[:player_choice]
+
+
+    computer_answer = options[computer_choice]
+    session['count'] += 1
+
+    @count = session['count']
+
+    if player_choice == 'rock' && computer_answer == 'scisor'
+      #player win
+      @result = 'You win!'
+    elsif player_choice == 'paper' && computer_answer == 'rock'
+      #player win
+      @result = 'You win!'
+    elsif player_choice == 'scisor' && computer_answer == 'paper'
+      #player win
+      @result = 'You win!'
+    elsif computer_answer == 'rock' && player_choice == 'scisor'
+     #computer win
+     @result = 'You lose!'
+    elsif computer_answer == 'paper' && player_choice == 'rock'
+     #computer win
+     @result = 'You lose!'
+    elsif computer_answer == 'scisor' && player_choice == 'paper'
+     #computer win
+     @result = 'You lose!'
+    else
+     @result = 'Tied!'
+   end
+  end
+end
